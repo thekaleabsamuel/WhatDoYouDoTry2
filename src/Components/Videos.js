cae71748a7acd502cfac46774ae927daca66624e
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-03-27 23:26:20 -0600

    go

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,7 +1,6 @@
 // Videos.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import React, { useEffect, useState } from 'react'; 
+ees
 function Videos() {
   const [video, setVideos] = useState([]);
 
