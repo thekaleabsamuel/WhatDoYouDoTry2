ffa79dbd4ea664f706abf0d4938f0f10977d322d
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-03-28 02:11:13 -0600

    fetch is working

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,20 +1,21 @@
 // Videos.js
 import React, { useEffect, useState } from 'react'; 
-ees
+
 function Videos() {
   const [video, setVideos] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3030/Archive')
-      .then(response => {
-        setVideos(response.data);
+      .then(response => response.json())
+      .then(data => {
+        setVideos(data);
       });
   }, []);
 
   return (
     <div>
       <h1>Videos</h1>
-      {video.map(videoItem => ( // Change to video
+      {video.map(videoItem => (
         <div key={videoItem.id}>
           <h2>{videoItem.title}</h2>
           <p>{videoItem.body}</p>
